a997c60af09977995e4fe6d845ad227523eb96c6
mac <zhangqi1@hellogeek.com>
2019-12-24 15:27:33 +0800

    添加演示的demo样例： \n / 1、路由示例; 2、带有传递参数的路由.

---
+++ commitChanbges1.sh
@@ -0,0 +1,7 @@
+
+git add .
+git commit -m "添加演示的demo样例： \n /
+1、路由示例; \
+2、带有传递参数的路由. \
+"
+git push -u origin master
--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,32 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page 2'),
+
+      // 注册路由表 路由注册信息
+      routes:{
+        "new_page":(context) => NewRoute(),
+        "tip_page":(context) => TipRoute(text: 'Flutter Demo Home Page'),
+        "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        "transParams_page":(context) => EchoRoute(),
+        "tip2": (context){
+          return TipRoute(text: ModalRoute.of(context).settings.arguments);},
+
+      } ,
+
+      //home: MyHomePage(title: 'Flutter Demo Home Page 2'),
+
+      /// 路由生成钩子
+      /// 注意，onGenerateRoute只会对命名路由生效。
+      onGenerateRoute:(RouteSettings settings){
+          return MaterialPageRoute(builder: (context){
+            String routeName = settings.name;
+            /// 如果访问的路由页需要登录，但当前未登录，则直接返回登录页路由，
+            /// 引导用户登录；其它情况则正常打开路由。
+            /// 导航到新路由
+            Navigator.pushNamed(context, routeName);
+          }
+          );
+        },
     );
   }
 }
@@ -111,6 +136,51 @@ class _MyHomePageState extends State<MyHomePage> {
                     }));
               },
             ),
+
+            /// 新路由传参
+            FlatButton(
+              child: Text("路由传参"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.push( context,
+                    MaterialPageRoute(builder: (context) {
+                      return TipRoute(text: "我是传值路由");
+                    }));
+              },
+            ),
+
+            /// 通过使用路由名字，完成页面跳转
+            FlatButton(
+              child: Text("通过路由名字，跳转界面"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.pushNamed(context, "new_page");
+              },
+            ),
+
+            /// 通过路由名字，跳转，并传递参数
+            FlatButton(
+              child: Text("通过路由名字，跳转，并传递参数"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.of(context).pushNamed("transParams_page", arguments: "hello world");
+              },
+            ),
+
+            /// 在路由表中接收参数传递
+            FlatButton(
+              child: Text("在路由表中接收参数传递"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.of(context).pushNamed("tip2", arguments: "hello world tip2");
+              },
+            ),
+
+
           ],
         ),
 
@@ -134,6 +204,111 @@ class NewRoute extends StatelessWidget {
       ),
       body: Center(
         child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+
+/// 路由传值
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,  // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("路由传参展示 Demo"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          // 打开`TipRoute`，并等待返回结果
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                  // 路由参数
+                  text: "我是提示xxxx",
+                );
+              },
+            ),
+          );
+          //输出`TipRoute`路由返回结果
+          print("路由返回值: $result");
+        },
+        child: Text("打开提示页"),
+      ),
+    );
+  }
+}
+
+/// 通过路由名字，跳转，并传递参数
+class EchoRoute extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    /// 获取路由参数
+    var args=ModalRoute.of(context).settings.arguments;
+    print(args);
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("通过路由名字跳转打印接收参数"),
+      ),
+      body: Center(
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '打印接收参数:',
+              ),
+
+              Text(
+                '传递的参数 $args',
+                style: Theme.of(context).textTheme.subtitle,
+              ),
+          ],
+        ),
       ),
     );
   }
